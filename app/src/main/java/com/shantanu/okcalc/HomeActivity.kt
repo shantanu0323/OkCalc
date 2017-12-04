1c9553aa3fca26ef618bc44c4ff4e54caf4a2196
Shantanu Pramanik <srs.shaan@gmail.com>
2017-12-04 23:56:58 +0530

    Basic Layout of the normal calculator

--- app/src/main/java/com/shantanu/okcalc/HomeActivity.kt
+++ app/src/main/java/com/shantanu/okcalc/HomeActivity.kt
@@ -8,5 +8,9 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        var message = "Shantanu"
     }
+
 }
+
